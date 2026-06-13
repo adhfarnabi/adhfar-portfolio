237b903d65d8bf902881cce2feac5e6cc4bf561d
Adhfar Nabi <adhfarnabi2020@gmail.com>
2026-06-13 10:35:40 +0530

    Fix API URL for Vercel

--- client/src/api.js
+++ client/src/api.js
@@ -1,10 +1,13 @@
+
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '/api' })
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL + '/api'
+})
 
 export const getPortfolioData = () => api.get('/data')
-export const getProjects      = () => api.get('/data/projects')
-export const getSkills        = () => api.get('/data/skills')
-export const sendContact      = (data) => api.post('/contact', data)
+export const getProjects = () => api.get('/data/projects')
+export const getSkills = () => api.get('/data/skills')
+export const sendContact = (data) => api.post('/contact', data)
 
 export default api
